27c5ba0bcde2f99530bdc45a4db66cb24236064f
martinjnolmedo <olmedo.molmedo@gmail.com>
2025-10-27 21:35:42 -0300

    Cambio en Vite para que funcione en github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-JS-pre-entrega-Mart-n-Olmedo/',
+  base: '/react-JS-pre-entrega-Martin-Olmedo/',
   plugins: [react()],
 })
